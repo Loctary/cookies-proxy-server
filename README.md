da825f069d00e01c3ce5e951a453f15197f5fd29
Vitaliy Titarenko <vitaliy.titarenko@gmail.com>
2018-12-06 23:32:24 +0200

    working code...

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# cookie-proxy-server
+# cookies-proxy-server
 The problem is to send cookies from localhost developer environment to remote API. Browsers don't send the cookies and don't allow to change cookie-field in headers if you are not in the same domain. So, the obvious answer is to redirect all requests-responses to remote API through the local proxy server.
---
+++ index.js
@@ -0,0 +1,30 @@
+// https://www.npmjs.com/package/http-proxy-middleware
+// include dependencies
+
+var express = require('express')
+var proxy = require('http-proxy-middleware')
+require('dotenv').load();
+
+// proxy middleware options
+var options = {
+  target: process.env.PROXY_TO, // target host
+  changeOrigin: true, // needed for virtual hosted sites
+  ws: true, // proxy websockets
+  pathRewrite: {
+    // '^/api/old-path': '/api/new-path', // rewrite path
+    // '^/api/remove/path': '/path' // remove base path
+  },
+  router: {
+    // when request.headers.host == 'dev.localhost:3000',
+    // override target 'http://www.example.org' to 'http://localhost:8000'
+    // 'dev.localhost:3000': 'http://localhost:8000'
+  }
+}
+ 
+// create the proxy (without context)
+var exampleProxy = proxy(options)
+ 
+// mount `exampleProxy` in web server
+var app = express()
+app.use('', exampleProxy)
+app.listen(process.env.PROXY_PORT)
